c446caa2dd98418ef6b517d165ecf33ffb1006ed
Noah Gibson <ngibson.dev@gmail.com>
2024-10-04 08:16:55 -0600

    Add nav tabs

---
+++ app/(tabs)/races.tsx
@@ -0,0 +1,38 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function Races() {
+  return (
+    <>
+      <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Ionicons size={310} name="code-slash" style={styles.reactLogo} />}>
+        <ThemedText>Races</ThemedText>
+      </ParallaxScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
