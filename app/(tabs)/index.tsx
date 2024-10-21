ead7d45342d6d05e29c24050928dbe18b47b3a64
Noah Gibson <ngibson.dev@gmail.com>
2024-10-21 13:19:03 -0600

    Add hooks for data fetch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,44 +1,107 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { useState, useEffect } from 'react';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { Race } from '@/components/Race'
 
-const key = process.env.EXPO_PUBLIC_KEY
-const currentYear = new Date().getFullYear();
 
-async function getRaces() {
-  try {
-    const response = await fetch(`https://v1.formula-1.api-sports.io/races?season=${currentYear}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "v1.formula-1.api-sports.io",
-        "x-rapidapi-key": key
+// async function getRaces() {
+//   const []
+//   const currentYear = new Date().getFullYear();
+//   const key = process.env.EXPO_PUBLIC_KEY
 
+//   try {
+//     const response = await fetch(`https://v1.formula-1.api-sports.io/races?season=${currentYear}`, {
+//       "method": "GET",
+//       "headers": {
+//         "x-rapidapi-host": "v1.formula-1.api-sports.io",
+//         "x-rapidapi-key": key
+
+//       }
+//     })
+
+//     if(!response.ok) {
+//       throw new Error('No races for you!')
+//     }
+
+//     return response
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// let races = getRaces()
+// console.log(races)
+
+const sampleData: any = [
+  {
+    id: 1857,
+    competition: {
+      id: 2,
+      name: "Bahrain Grand Prix",
+      location: {
+      country: "Bahrain",
+      city: "Sakhir",
       }
-    })
-
-    if(!response.ok) {
-      throw new Error('No races for you!')
-    }
-
-    const races = await response.json();
-    console.log(races.response)
-    return races.response
-  } catch (error) {
-    console.error(error)
+    },
+    circuit: {
+      id: 2,
+      name: "Bahrain International Circuit",
+      image: "https://media.api-sports.io/formula-1/circuits/2.png",
+    },
+    season: 2024,
+    type: "Race",
+    laps: {
+    current: null,
+    total: 57,
+    },
+    fastest_lap: {
+    driver: {
+    id: 25,
+    },
+    time: "1:32.608",
+    },
+    distance: "308.5 Kms",
+    timezone: "utc",
+    date: "2024-03-02T15:00:00+00:00",
+    weather: null,
+    status: "Completed",
   }
-}
-
-const races = getRaces()
+]
 
 export default function HomeScreen() {
+  const [races, setRaces] = useState()
+
+  useEffect(() => {
+    const getRaces = async () => {
+      const currentYear = new Date().getFullYear();
+      const key = process.env.EXPO_PUBLIC_KEY
+      const data = await fetch(`https://v1.formula-1.api-sports.io/races?season=${currentYear}`, {
+          "method": "GET",
+          "headers": {
+            "x-rapidapi-host": "v1.formula-1.api-sports.io",
+            "x-rapidapi-key": key
+    
+          }
+        })
+
+        const races = await data.json()
+        setRaces(races.response)
+    }
+
+    getRaces()
+    .catch(console.error)
+  }, [])
+
+  console.log(races)
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.reactLogo} />}>
-        {/* <Race race={races}></Race> */}
+        <Race races={races}></Race>
     </ParallaxScrollView>
   );
 }
--- components/Race.tsx
+++ components/Race.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { PropsWithChildren, useState } from 'react';
+import { StyleSheet, TouchableOpacity, useColorScheme, FlatList, Text } from 'react-native';
 
-export default function Race(props:any) {
-    return (
-      <View>
-        <Text>{props}</Text>
-        <Text>Am I rendering?</Text>
-      </View>
-    );
-  };
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/Colors';
+
+export function Race({races}: any) {
+
+console.log('this is the race', races)
+
+  return (
+    <Text style={styles.container}>{races[6].competition.name}</Text>
+    // <Text>{races.map((race: { competition: { name: any; }; }) => race.competition.name)}</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+    },
+  container: {
+    backgroundColor: 'white',
+  },
+  heading: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
+    color: '#808080',
+  },
+  content: {
+    marginTop: 6,
+    marginLeft: 24,
+  },
+  color: {
+    color: 'white',
+  }
+});
