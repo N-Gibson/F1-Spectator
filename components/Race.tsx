c848c668195b772db03da42d3d3d11d88e093d80
Noah Gibson <ngibson.dev@gmail.com>
2024-10-22 14:02:44 -0600

    Clean up race component

--- components/Race.tsx
+++ components/Race.tsx
@@ -1,10 +1,4 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { PropsWithChildren, useState,} from 'react';
-import { StyleSheet, TouchableOpacity, useColorScheme, FlatList, Text, View, SafeAreaView } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Colors } from '@/constants/Colors';
+import { StyleSheet, FlatList, Text, View, SafeAreaView } from 'react-native';
 
 export function Race({races}: any) {
 
@@ -20,33 +14,11 @@ export function Race({races}: any) {
     <SafeAreaView>
       <FlatList data={races} renderItem={({ item }) => <Race raceName={item.competition.name}/>}/>
     </SafeAreaView>
-
-    // <Text style={styles.container}>{races[6].competition.name}</Text>
-    // <Text>{races.map((race: { competition: { name: any; }; }) => race.competition.name)}</Text>
   );
 }
 
 const styles = StyleSheet.create({
-headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-    },
   container: {
     backgroundColor: 'white',
-  },
-  heading: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 6,
-    color: '#808080',
-  },
-  content: {
-    marginTop: 6,
-    marginLeft: 24,
-  },
-  color: {
-    color: 'white',
   }
 });
